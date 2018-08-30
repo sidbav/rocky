14998ee5655610b269367268a6910523d4e81749
Sidharth Baveja <sidharth88@hotmail.ca>
2018-08-29 21:21:22 -0400

    package.json, start time-now

---
+++ src/intents/timeNow.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//loading all of the environment variables from .env file
+require('dotenv').config();
+
+
+module.exports = (data, callback) => {
+
+    //ensure we get the correct intent
+    if (data.intent[0].value != 'time-now')
+        return callback(new Error(`Expected time intent, got ${data.intent[0].value} instead`));
+} 
