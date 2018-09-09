bf00657e9386e8ac8af99cfba53e5cef78e7df0a
Sidharth Baveja <sidharth88@hotmail.ca>
2018-09-09 12:46:30 -0400

    title in html

--- src/slackClient.js
+++ src/slackClient.js
@@ -59,7 +59,11 @@ module.exports = (app) => {
     app.use(passport.initialize());
 
     app.get('/', (req, res) => {
-        res.send('<a href="/auth/slack"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>');
+        res.send(
+            '<head><title>Rocky</title></head><a href="/auth/slack"><img alt="Add to Slack" height="40" \
+            width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" \
+            srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>'
+        );
     });
 
     app.get('/auth/slack', passport.authenticate('slack', {
